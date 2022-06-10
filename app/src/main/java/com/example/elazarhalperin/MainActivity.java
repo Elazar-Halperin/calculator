726ea8b674a08b028b643c4ac3edff61ef98b755
Elazar Halperin <elazarhalper@gmail.com>
2022-06-10 12:05:16 +0300

    added ur mom

--- app/src/main/java/com/example/elazarhalperin/MainActivity.java
+++ app/src/main/java/com/example/elazarhalperin/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.elazarhalperin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import java.sql.SQLXML;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("hi", "hi");
+        Button btn;
     }
 }
